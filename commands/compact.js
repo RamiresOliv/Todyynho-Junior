035facd78b1cd9aae7f540ed57bf5f75edc4f107
gabrielramires <69019082+gabrielramires@users.noreply.github.com>
2020-12-09 22:07:54 -0300

    Delete compact.js

--- commands/compact.js
+++
@@ -1,3 +0,0 @@
-const Discord = require('discord.js') 
-exports.run = async (client, message, args) => {const embed = new Discord.MessageEmbed() .setDescription("esse comando é SUPER compacto") .setImage('https://cdn.discordapp.com/attachments/727984820269088798/771100223254560788/unknown.png') .setColor('#ff0d00') .setFooter(`muito compacto!`) 
-message.channel.send(`${message.author}`, embed)}
